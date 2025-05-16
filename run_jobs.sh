b5565cdf6e513d0a6aab65930956c17cb1e9f7a6
Neo-X <glen.berseth@mila.quebec>
2025-05-16 12:19:05 -0400

    Working on jobs

--- run_jobs.sh
+++ run_jobs.sh
@@ -1,5 +1,8 @@
 #/bin/bash
 ## Code to run all the jobs for this codebase 
 
-sbatch --array=1-3 --export=ALL,ARGSS='--env_name Craftax-Symbolic-v1' launchGPU.sh
+# sbatch --array=1-3 --export=ALL,ARGSS='--env_name Craftax-Symbolic-v1' launchGPU.sh
+# sbatch --array=1-3 --export=ALL,ARGSS='--env_name Craftax-Symbolic-v1' launchGPU.sh
+sbatch --array=1-4 --export=ALL,ARGSS='--env_name Craftax-Symbolic-v1 --train_icm' launchGPU.sh
+sbatch --array=1-4 --export=ALL,ARGSS='--env_name Craftax-Symbolic-v1 --use_e3b' launchGPU.sh
 
