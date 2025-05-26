f9fc9481d2473ed34272f7711cf3e019b33bd9cf
Neo-X <glen.berseth@mila.quebec>
2025-05-26 12:16:06 -0400

    Adding small jobs

--- run_jobs.sh
+++ run_jobs.sh
@@ -4,6 +4,7 @@
 # sbatch --array=1-3 --export=ALL,ARGSS='--env_name Craftax-Symbolic-v1' launchGPU.sh
 # sbatch --array=1-3 --export=ALL,ARGSS='--env_name Craftax-Symbolic-v1' launchGPU.sh
 # sbatch --array=1-4 --export=ALL,ARGSS='--env_name Craftax-Symbolic-v1 --train_icm' launchGPU.sh
-sbatch --array=1-3 --export=ALL,ALG='ppo.py',ARGSS='--env_name Craftax-Symbolic-v1 --ent_coef=0.1 --use_e3b --train_icm --icm_reward_coeff=0' launchGPU.sh
-sbatch --array=1-3 --export=ALL,ALG='ppo_rnd.py',ARGSS='--env_name Craftax-Symbolic-v1 --ent_coef=0.1 --use_rnd' launchGPU.sh
+sbatch --array=1-3 --export=ALL,ALG='ppo.py',ARGSS='--env_name Craftax-Symbolic-v1' launchGPU.sh
+# sbatch --array=1-3 --export=ALL,ALG='ppo.py',ARGSS='--env_name Craftax-Symbolic-v1 --ent_coef=0.1 --use_e3b --train_icm --icm_reward_coeff=0' launchGPU.sh
+# sbatch --array=1-3 --export=ALL,ALG='ppo_rnd.py',ARGSS='--env_name Craftax-Symbolic-v1 --ent_coef=0.1 --use_rnd' launchGPU.sh
 
